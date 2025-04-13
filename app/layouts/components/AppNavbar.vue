e606415d4c92e05e4e316ee14b6b249547dbccb3
Cyanhall <cyanhalldev@gmail.com>
2025-04-13 20:01:04 +0800

    feat: enhance navbar styling with backdrop blur and adjusted background opacity

--- app/layouts/components/AppNavbar.vue
+++ app/layouts/components/AppNavbar.vue
@@ -5,7 +5,7 @@ const localePath = useLocalePath()
 
 <template>
   <!-- Navigation -->
-  <nav class="fixed h-16 top-0 left-0 right-0 z-50 bg-white dark:bg-gray-900">
+  <nav class="fixed h-16 top-0 left-0 right-0 z-50 bg-white/70 dark:bg-gray-900/70 backdrop-blur-md border-b border-gray-200 dark:border-gray-800">
     <UContainer class="flex h-full items-center justify-between">
       <NuxtLink
         :to="localePath('/')"
