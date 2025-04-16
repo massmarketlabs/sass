4a933a9b8940847b062d28ed935bc281c40f69ab
Cyanhall <cyanhalldev@gmail.com>
2025-04-16 09:36:18 +0800

    refactor: improve performance logging by using performance.now() and format timestamp

--- server/middleware/common.ts
+++ server/middleware/common.ts
@@ -1,19 +1,23 @@
 export default defineEventHandler(async (event) => {
-  const start = Date.now()
+  const start = performance.now()
   const { req, res } = event.node
 
   await res.on('finish', () => {
-    // Release db connection
-    if (event.context.db) {
-      event.context.db.$client.release()
-    }
-
-    const duration = Date.now() - start
+    const duration = Math.round(performance.now() - start)
     const statusCode = res.statusCode
     const method = req.method
     const url = req.url
 
+    const timestamp = new Date().toLocaleString('en-US', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
+    })
     // Access Log
-    console.log(`[${new Date().toISOString()}] ${method} ${url} ${statusCode} - ${duration}ms`)
+    console.log(`[${timestamp}] ${method} ${url} ${statusCode} - ${duration}ms`)
   })
 })
